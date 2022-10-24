d80dd70a717d8c30d933754b2547527684462bf6
kushagraahire55 <KUSHAGRAAHIRE55@GMAIL.COM>
2022-10-24 21:18:39 +0530

    Login Signup Front End issues solved

---
+++ app/src/main/java/com/company/petadoptionapp/ViewLostPets.java
@@ -0,0 +1,14 @@
+package com.company.petadoptionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewLostPets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_lost_pets);
+    }
+}
