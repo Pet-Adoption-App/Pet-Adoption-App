277aa3ae08ca782c89945fce560ab7560ea75cd6
kushagraahire55 <KUSHAGRAAHIRE55@GMAIL.COM>
2022-10-31 12:04:40 +0530

    User can call Pet Owner

--- app/src/main/java/com/company/petadoptionapp/ViewPets.java
+++ app/src/main/java/com/company/petadoptionapp/ViewPets.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,7 @@ public class ViewPets extends AppCompatActivity {
     String otherUserKey,otherUsername;
     String otherImage;
     String currentUser;
+    String userPhone;
 
     FirebaseAuth auth;
     FirebaseUser firebaseUser;
@@ -89,8 +91,10 @@ public class ViewPets extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 otherUsername = snapshot.child(otherUserKey).child("userName").getValue().toString();
                 otherImage = snapshot.child(otherUserKey).child("image").getValue().toString();
-                if(firebaseUser != null)
-                currentUser = snapshot.child(firebaseUser.getUid()).child("userName").getValue().toString();
+                if(firebaseUser != null) {
+                    currentUser = snapshot.child(firebaseUser.getUid()).child("userName").getValue().toString();
+                    userPhone = snapshot.child(firebaseUser.getUid()).child("userMobile").getValue().toString();
+                }
             }
 
             @Override
@@ -133,5 +137,25 @@ public class ViewPets extends AppCompatActivity {
             }
         });
 
+        btnCallViewPets.setOnClickListener(view -> {
+            if(firebaseUser != null){
+                if(currentUser != otherUsername){
+                    dailPhone(userPhone);
+                }else {
+                    Toast.makeText(this, "Not allowed", Toast.LENGTH_SHORT).show();
+                }
+
+            }else{
+                Toast.makeText(this, "Please Login", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void dailPhone(String userPhone) {
+        String uri = "tel:"+userPhone.trim();
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(uri));
+        startActivity(intent);
     }
 }
